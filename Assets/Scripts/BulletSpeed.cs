25f332fe9b42820f0a8066e15caa49b4ce6754a9
Dev <d.trivedi@saedubai.com>
2021-11-18 15:04:46 +0400

    Bullet speed now destroys properly
    I don't think anything changed here but might as well just in case

--- Assets/Scripts/BulletSpeed.cs
+++ Assets/Scripts/BulletSpeed.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class BulletSpeed : MonoBehaviour
 {
-    private Rigidbody bulletRB;
-    // Start is called before the first frame update
-    void Start()
-    {
-        bulletRB = GetComponent<Rigidbody>();
+	private Rigidbody bulletRB;
+	// Start is called before the first frame update
+	void Start()
+	{
+		bulletRB = GetComponent<Rigidbody>();
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        Speed();
-        if(bulletRB.transform.position.z > 15)
-        {
-            Destroy(this.gameObject);
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		Speed();
+		if (bulletRB.transform.position.z > 15)
+		{
+			Destroy(this.gameObject);
+		}
+	}
 
-    void Speed()
-    {
-        float x = 0;
-        float y = 0;
-        bulletRB.velocity = new Vector3(x, y, 10);
-    }
+	void Speed()
+	{
+		float x = 0;
+		float y = 0;
+		bulletRB.velocity = new Vector3(x, y, 10);
+	}
 }
